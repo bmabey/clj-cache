3a2bae51799f2f35ccd28b082db04497af57246c
Ben Mabey <ben@benmabey.com>
2012-01-27 09:19:09 -0700

    uses lein multi

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
 (defproject clj-cache "0.0.4"
   :description "A resettable memoize."
   :dependencies []
-  :dev-dependencies [[org.clojure/clojure "1.2.0"]
-                     [org.clojure/clojure-contrib "1.2.0"]
-                     [swank-clojure "1.2.1"]]
+  :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]]
+               "1.2" [[org.clojure/clojure "1.2.1"]]}
+  :dev-dependencies [[swank-clojure "1.3.4"]]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"})
-
